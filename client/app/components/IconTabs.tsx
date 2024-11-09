b52839044e8ffe494eb828173f0835c28ffa30c0
Alan Kalkan <alan0613@live.fr>
2024-11-10 00:44:05 +0100

    testing

--- client/app/components/IconTabs.tsx
+++ client/app/components/IconTabs.tsx
@@ -4,16 +4,17 @@ import { YStack } from "tamagui";
 export default function IconTabs() {
   return (
     <YStack
-    backgroundColor="chartreuse"
+      backgroundColor="chartreuse"
       position="absolute"
       bottom={0}
       left={0}
       right={0}
-      height={80}
+      height={65}
       flexDirection="row"
       justifyContent="space-between"
       paddingHorizontal="$8"
       paddingVertical="$2"
+      paddingBottom="$3"
     >
       <House size={35} color="black" />
       <User size={35} color="black" />
