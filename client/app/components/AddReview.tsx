b472b75053bf73f9c2a230b5c16cb02c86f919f9
Alan Kalkan <alan0613@live.fr>
2024-11-23 22:01:18 +0100

    fix

--- client/app/components/AddReview.tsx
+++ client/app/components/AddReview.tsx
@@ -1,32 +1,14 @@
 import React, { useState } from 'react';
-import { View, Input, Button, Popover } from 'tamagui';
-import { reviewService } from '../services/api/reviewService';
-import { Modal } from 'react-native';
+import { View, Text, Input, Button, Popover } from 'tamagui';
+
+import { PlusCircle } from 'lucide-react-native';
 
 export const AddReview = () => {
-  const [content, setContent] = useState('');
 
-  const handleAddReview = () => {
-    if (content.trim()) {
-      reviewService.addReview({ comment: content, rating: 5 });
-      setContent('');
-    }
-  };
-  console.log("content", content)
   return (
     <View>
-      <Popover>
-        <Popover.Trigger>
-          <Button>Add Review</Button>
-        </Popover.Trigger>
-        <Popover.Content>
-          <Input
-            value={content}
-            onChangeText={setContent}
-            placeholder="Write your review..."
-          />
-        </Popover.Content>
-      </Popover>
+      <Text>Add a review</Text>
+      <PlusCircle width={30} height={30} color="black" />
     </View>
   );
 };
