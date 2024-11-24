27525ce526aab2ade6cddd68146b5e1215d3b830
Alan Kalkan <alan0613@live.fr>
2024-11-24 12:10:40 +0100

    back for auth

--- server/src/controllers/users/usersController.ts
+++ server/src/controllers/users/usersController.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from 'express';
 import { PrismaClient } from '@prisma/client';
 import { v4 as uuidv4 } from 'uuid';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 const prisma = new PrismaClient();
 
 // CREATE
@@ -19,6 +21,22 @@ export const addUser = async (req: Request, res: Response): Promise<void> => {
     res.status(500).json({ error: `Error adding user: ${user.name} ${user.last_name}: ${error.message}` });
   }
 };
+
+export const registerUser = async (req: Request, res: Response): Promise<void> => {
+  const { name, last_name, email, password, gender } = req.body;
+  const id = uuidv4();
+  const date = new Date();
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const user = await prisma.user.create({
+    data: { id, name, last_name, email, password: hashedPassword, gender, created_at: date },
+  });
+  try {
+    console.log('user',user);
+    res.json(user);
+  } catch (error: any) {
+    res.status(500).json({ error: `Error adding user: ${user.name} ${user.last_name}: ${error.message}` });
+  }
+}
 
 // READ
 export const getUsers = async (req: Request, res: Response): Promise<void> => {
@@ -39,6 +57,33 @@ export const getUserById = async (req: Request, res: Response): Promise<void> =>
     res.status(500).json({ error: `Internal server error: ${error.message}` });
   }
 };
+
+export const loginUser = async (req: Request, res: Response): Promise<void> => {
+  const { email, password } = req.body;
+
+  console.log(req.body);
+  try {
+    const user = await prisma.user.findUnique({ where: { email } });
+
+    if (!user) {
+      res.status(401).json({ error: 'Invalid credentials' });
+      return;
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      res.status(401).json({ error: 'Invalid credentials' });
+      return;
+    }
+
+    const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET as string, { expiresIn: '1h' });
+
+    res.json({ message: 'Login successful', token });
+  } catch (error: any) {
+    console.error('Error logging in:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+}
 
 // UPDATE
 export const updateUser = async (req: Request, res: Response): Promise<void> => {
--- server/src/routes/users/usersRoutes.ts
+++ server/src/routes/users/usersRoutes.ts
@@ -1,11 +1,15 @@
 import express from 'express';
-import { addUser, deleteUser, getUserById, getUsers, updateUser } from '../../controllers/users/usersController';
+import { addUser, deleteUser, getUserById, getUsers, loginUser, registerUser, updateUser } from '../../controllers/users/usersController';
 
 const router = express.Router();
 
 router.post('/addUser', addUser);
 
+router.post('/registerUser', registerUser);
+
 router.get('/getUsers', getUsers);
+
+router.post('/loginUser', loginUser);
 
 router.get('/getUserById/:id', getUserById);
 
