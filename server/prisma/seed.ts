a1c12aa16b5f877bbecfb8f7329d7d6bdda228bf
Alan Kalkan <alan0613@live.fr>
2024-12-01 18:20:40 +0100

    chore: seeding prisma db

---
+++ server/prisma/seed.ts
@@ -0,0 +1,26 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+async function main() {
+    await prisma.user.create({
+        data: {
+            id: 'here-the-uuid',
+            email: 'test@prisma.com',
+            name: 'test',
+            last_name: 'test',
+            password: 'test',
+            gender: 'male',
+        }
+    })
+}
+
+main()
+.then(async () => {
+    await prisma.$disconnect();
+})
+.catch(async (e) => {
+        console.error(e);
+        await prisma.$disconnect();
+        process.exit(1);
+    });
